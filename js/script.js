998102565343b8a18e2e08cd97b3d6ee83d2027f
Kolya <kovalenkokola66@gmail.com>
2023-03-14 15:44:06 +0200

    Пирамидка

---
+++ js/script.js
@@ -0,0 +1,36 @@
+'use strict'
+/*cycle*/
+let num = 50
+/*
+do {
+    console.log('Hello')
+}
+while (num < 15){
+    num++
+}*/
+
+/*for (let i = 1; i < 10; i++) {
+    if(i===6){
+        console.log('trim')
+        continue;
+    }
+    console.log(i)
+}*/
+
+/*nested cycle */
+/*for (let i = 0; i < 3; i++) {
+    console.log(i)
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}*/
+let result = ''
+let test = '*'
+const length = 8
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        result += '*'
+    }
+    result += '\n'
+}
+console.log(result)
